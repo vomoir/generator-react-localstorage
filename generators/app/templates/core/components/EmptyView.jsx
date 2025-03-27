fa9db6b011ea15edb4605a101fc7256166d7be66
Tony Edwards <anthonybedwards@optusnet.com.au>
2025-03-28 09:26:24 +1100

    removed tailwind references

--- generators/app/templates/core/components/EmptyView.jsx
+++ generators/app/templates/core/components/EmptyView.jsx
@@ -1,6 +1,6 @@
 export default function EmptyView() {
   return (
-    <section className="p-11 items-center">
+    <section className="empty-state">
       <h2>Empty Packing List</h2>
       <p>Start by adding items you absolutely wouldn&apos;t want to forget! </p>
     </section>
